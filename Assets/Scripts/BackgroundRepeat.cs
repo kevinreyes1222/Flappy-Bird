256b1e6f8332d0cca373fa9ab4dcb2924c7502f5
Kevin Reyes <70299081+kevinreyes1222@users.noreply.github.com>
2022-03-16 18:50:39 -0400

    Scrolling system
    created a scrolling system to scroll the background image

---
+++ Assets/Scripts/BackgroundRepeat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeat : MonoBehaviour
+{
+	
+	private float spriteWidth;
+	
+    void Start()
+    {
+    	BoxCollider2D groundCollider = GetComponent<BoxCollider2D>();
+    	spriteWidth = groundCollider.size.x;
+    }
+
+    void Update()
+    {
+	    if (transform.position.x < -spriteWidth)
+	    {
+	    	ResetPosition();
+	    }
+    }
+    
+	void ResetPosition(){
+		transform.Translate(new Vector3(2 * spriteWidth, 0f,0f));
+	}
+}
---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+	
+	private Rigidbody2D rb; 
+	[SerializeField] private float speed = 2.5f;
+	
+    void Start()
+    {
+	    rb = GetComponent<Rigidbody2D>();
+	    rb.velocity = Vector2.left * speed;
+    }
+
+    
+    void Update()
+    {
+	    if (GameManager.Instance.isGameOver)
+	    {
+	    	rb.velocity = Vector2.zero;
+	    }
+    }
+}
