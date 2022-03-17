b8bbfac44b70e108ef64d86f45d7b6b06cbab683
Kevin Reyes <70299081+kevinreyes1222@users.noreply.github.com>
2022-03-16 20:20:54 -0400

    Object pooling implimentation
    implementation of object pooling to allow reuse of automatically created objects

---
+++ Assets/Scripts/ObstaclesPool.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class ObstaclesPool : MonoBehaviour
+{
+   
+	[SerializeField] private GameObject obstaclePrefab;
+	[SerializeField] private int poolSize = 5;
+	[SerializeField] private float spawnTime = 2.5f;
+	[SerializeField] private float xSpawnPos = 12;
+	
+	[SerializeField]private float minYPosition = -2f;
+	[SerializeField]private float maxYPosition = 3f;
+	
+	private float timeElapsed;
+	
+	private int obstacleCount;
+	
+	
+	private GameObject[] obstacles;
+   
+    void Start()
+    {
+	    obstacles = new GameObject[poolSize];
+	    
+	    for (int i = 0; i < poolSize; i++) {
+	    	obstacles[i] = Instantiate(obstaclePrefab);
+	    	obstacles[i].SetActive(false);
+	    }
+    }
+
+    
+    void Update()
+    {
+	    timeElapsed += Time.deltaTime;
+	    if (timeElapsed > spawnTime && !GameManager.Instance.isGameOver)
+	    {
+	    	SpawnObstacle();
+	    }
+    }
+    
+	void SpawnObstacle(){
+		timeElapsed = 0f;
+		
+		float ySpawnPos = Random.Range(minYPosition,maxYPosition);
+		Vector2 spawnPos = new Vector2(xSpawnPos,ySpawnPos);
+		obstacles[obstacleCount].transform.position = spawnPos;
+		
+		if (!obstacles[obstacleCount].activeSelf)
+		{
+			obstacles[obstacleCount].SetActive(true);
+			
+		}
+		
+		obstacleCount++;
+		
+		if (obstacleCount == poolSize)
+		{
+			obstacleCount = 0;
+		}
+							
+	}
+}
